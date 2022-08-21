10fc57ab06f6e771e72b26edebed50e474ef0c3a
TCdotmake <tommyjchen@hotmail.com>
2022-08-21 12:07:45 -0400

    createMain(x,y) added, need to add bound check

--- html.js
+++ html.js
@@ -4,5 +4,23 @@ function createBox() {
   return box;
 }
 
+function createRow(num) {
+  const row = document.createElement("div");
+  row.classList.add("row");
+  for (let i = 0; i < num; i++) {
+    row.insertAdjacentElement("beforeend", createBox());
+  }
+  return row;
+}
+
+function createGrid(x, y) {
+  const main = document.createElement("div");
+  main.classList.add("main");
+  for (let i = 0; i < y; i++) {
+    main.insertAdjacentElement("beforeend", createRow(x));
+  }
+  return main;
+}
+
 const main = document.getElementById("mainContainer");
-main.insertAdjacentElement("beforeend", createBox());
+main.insertAdjacentElement("beforeend", createGrid(16, 16));
